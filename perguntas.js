073fc887ccb3139d37610517d1a889369b033a40
jvrazera <133227780+dubz27@users.noreply.github.com>
2024-10-21 07:41:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'artes',
+    'o que seria o estilo de pintura do claude monet?',
+    'seria uma pintura a base de tinta a oleo'
+)
+
+criaCartao(
+    'artes',
+    'quantos quadros o van gogh vendeu em sua vida?',
+    'apenas um em toda sua vida apesar de ter feito mais de 400 quadros em toda sua vida'
+)
+
+criaCartao(
+    'artes',
+    'por que a coloração da obras de  arte do van gogh são tão puxadas para o amarelo?',
+    'ele tinha depressão por isto ele via as cores mais amareladas'
+)
+
+
+criaCartao(
+    'artes',
+    'em que ano van gogh nasceu?',
+    'nasceu no dia 30 de março de 1853'
+)
